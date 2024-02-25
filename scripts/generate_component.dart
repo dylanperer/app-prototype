1b1604b964e10f61b7457ef934084a05f3a091aa
shalind <shalindperer@proton.me>
2024-02-25 14:04:30 +1300

    generate script update

--- scripts/generate_component.dart
+++ scripts/generate_component.dart
@@ -8,32 +8,32 @@ void main(List<String> arguments) {
   }
 
   final componentName = arguments[0];
-  bool isScreen = false;
 
-  if (arguments[1] == '-s') {
-    isScreen = true;
-  }
 
-  Directory componentDirectory = isScreen
-      ? Directory('../lib/screens/$componentName')
-      : Directory('../lib/components/$componentName');
 
-  if (componentDirectory.existsSync()) {
-    print('Directory already exists.');
-    return;
-  }
+
+
 
   try {
+
+    var x = Generate(componentName, arguments);
+
+    Directory componentDirectory = Directory('../lib/${x.result.fileType}s/$componentName');
+
+    if (componentDirectory.existsSync()) {
+      print('Directory already exists.');
+      return;
+    }
+
     componentDirectory.createSync(recursive: true);
 
     final componentFile =
-        File('${componentDirectory.path}/${componentName}_component.dart');
+        File('${componentDirectory.path}/${componentName}_${x.result.fileType}.dart');
     final viewFile =
         File('${componentDirectory.path}/${componentName}_view.dart');
 
-    componentFile.writeAsStringSync(
-        componentFileContent(componentName, isScreen ? 'screen' : 'component'));
-    viewFile.writeAsStringSync(viewFileContent(componentName));
+    componentFile.writeAsStringSync(x.result.type);
+    viewFile.writeAsStringSync(x.result.view);
 
     print('Component files created successfully.');
   } catch (e) {
@@ -41,13 +41,37 @@ void main(List<String> arguments) {
   }
 }
 
-String componentFileContent(String name, String type) {
-  return '''
-import 'package:flutter/material.dart';
-part '${name.toLowerCase()}_view.dart';
+class Generate {
+  final String name;
+  final List<String> args;
 
-class ${name.toPascalCase()}${type.toPascalCase()} extends StatelessWidget {
-  const ${name.toPascalCase()}${type.toPascalCase()}({super.key});
+  Generate(this.name, this.args);
+
+  GenerateResult get result {
+    String type = '';
+    String view = '';
+    String fileType = '';
+
+    if (args.length == 2 && args[1] == '-s') {
+      type = generateType(name.toPascalCase(), 'screen');
+      view = generateView(name, 'screen');
+      fileType =  'screen';
+    } else {
+      type = generateType(name.toPascalCase(), 'component');
+      view = generateView(name, 'component');
+      fileType =  'component';
+    }
+
+    return GenerateResult(type, view, fileType);
+  }
+
+  String generateType(String name, String type) {
+    return '''
+import 'package:flutter/material.dart';
+part '${name.toParamCase()}_view.dart';
+
+class $name${type.toPascalCase()} extends StatelessWidget {
+  const $name${type.toPascalCase()}({super.key});
   
   @override
   Widget build(BuildContext context) {
@@ -55,14 +79,23 @@ class ${name.toPascalCase()}${type.toPascalCase()} extends StatelessWidget {
   }
 }
 ''';
-}
+  }
 
-String viewFileContent(String name) {
-  return '''
-part of '${name}_component.dart';
+  String generateView(String name, String type) {
+    return '''
+part of '${name}_${type.toLowerCase()}.dart';
 
-Widget _${name.replaceAll('_', '').toCamelCase()}View(${(name).replaceAll('_', '').toPascalCase()}Component component, BuildContext context) {
+Widget _${name.replaceAll('_', '').toCamelCase()}View(${(name).replaceAll('_', '').toPascalCase()}${type.toPascalCase()} $type, BuildContext context) {
   return Placeholder();
 }
 ''';
+  }
+}
+
+class GenerateResult {
+  final String type;
+  final String view;
+  final String fileType;
+
+  GenerateResult(this.type, this.view, this.fileType);
 }
